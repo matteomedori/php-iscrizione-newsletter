a6656ce581f519feb6da69c819310b5657781ebb
matteomedori <matteo.medori.97@gmail.com>
2024-01-10 15:40:28 +0100

    feat: la pagina mostra alert a seconda della mail inserita

---
+++ index.php
@@ -0,0 +1,48 @@
+<?php
+
+// user_mail salva la mail inserita dall'utente tramite form
+$user_email = $_GET['email'] ?? '';
+// alert_class indica la classe da dare all'alert da mostrare
+$alert_class = '';
+// $alert_message indica il messaggio da visualizzare nell'alert
+$alert_message = '';
+
+if($user_email === ''){
+    $show_alert = false;    
+}elseif(str_contains($user_email,'@') && str_contains($user_email,'.')){
+    $show_alert = true;
+    $alert_class = 'alert-success';
+    $alert_message = 'Hai inserito una mail corretta!';
+} else {
+    $show_alert = true;
+    $alert_class = 'alert-warning';
+    $alert_message = 'Attenzione! Hai inserito una mail sbagliata! La mail non contiene un . e una @';
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Iscrizione Newsletter</title>
+    <!-- Bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+</head>
+<body>
+    <h1 class="text-center my-2">Inserisci un indirizzo Email</h1>
+    <div class="container">
+        <form action="index.php" method="GET">
+            <div class="mb-3">
+                <label for="email" class="form-label">Email address</label>
+                <input type="text" class="form-control" id="email" name="email">
+            </div>
+            <button type="submit" class="btn btn-primary">Submit</button>
+        </form>
+        <?php if($show_alert): ?>
+        <div class="alert mt-2 <?php echo $alert_class ?>" role="alert">
+            <?php echo $alert_message ?>
+        </div>
+        <? endif ?>
+    </div>
+</body>
+</html>
